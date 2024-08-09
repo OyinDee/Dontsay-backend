342754a121e45471d102d8b3bce6b08d2e057785
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-09 16:35:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const express= require('express')
 const app = express();
 // const bodyParser=require('body-parser')
 const mongoose = require('mongoose')
-// const { MongoClient, ServerApiVersion } = require('mongodb')
-// const url=process.env.URI
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const url=process.env.URI
 const bcryptjs= require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
@@ -14,24 +14,24 @@ const cors = require('cors')
 // app.use(bodyParser.json({limit: '50mb'}));
 // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
-// app.use(express.urlencoded({limit: '50mb'}))
-// mongoose.Query.prototype.timeout = 20000
-// mongoose.connect(url, (err)=>{
-//     if (err) {
-//         console.log(err.message);
-//         console.log("Error in mongodb connection");
-//     }
-//     else{
-//         console.log("MONGODB has connected");
-//     }
-// })
+app.use(express.urlencoded({limit: '50mb'}))
+mongoose.Query.prototype.timeout = 20000
+mongoose.connect(url, (err)=>{
+    if (err) {
+        console.log(err.message);
+        console.log("Error in mongodb connection");
+    }
+    else{
+        console.log("MONGODB has connected");
+    }
+})
 
 app.post("/user/create", create);
 app.post("/user/login", login);
 app.post("/get", getMessages)
 app.post("/send", sendMessage)
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("hello everyone")
 })
 
 app.listen(5000,( )=>{
