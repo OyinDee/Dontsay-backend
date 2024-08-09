3a401be35ef8c660c15d3cc27120fba6b5b3450c
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-09 16:33:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const {login, getMessages, sendMessage, create}=require('./Controllers/User.cont
 const PORT = process.env.PORT || 5000
 const express= require('express') 
 const app = express();
-const bodyParser=require('body-parser')
+// const bodyParser=require('body-parser')
 const mongoose = require('mongoose')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const url=process.env.URI
+// const { MongoClient, ServerApiVersion } = require('mongodb')
+// const url=process.env.URI
 const bcryptjs= require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
-app.use(express.urlencoded({limit: '50mb'}))
-mongoose.Query.prototype.timeout = 20000
-mongoose.connect(url, (err)=>{
-    if (err) {
-        console.log(err.message);
-        console.log("Error in mongodb connection");
-    }
-    else{
-        console.log("MONGODB has connected");
-    }
-})
+// app.use(express.urlencoded({limit: '50mb'}))
+// mongoose.Query.prototype.timeout = 20000
+// mongoose.connect(url, (err)=>{
+//     if (err) {
+//         console.log(err.message);
+//         console.log("Error in mongodb connection");
+//     }
+//     else{
+//         console.log("MONGODB has connected");
+//     }
+// })
 
 app.post("/user/create", create);
 app.post("/user/login", login);
@@ -34,6 +34,7 @@ app.get("/",(req,res)=>{
     res.send("hello")
 })
 
-app.listen(PORT,( )=>{
-    console.log(`app listening on PORT ${PORT}`)
+app.listen(5000,( )=>{
+    // console.log(`app listening on PORT ${PORT}`)
+    console.log('yeeeeeeeeeeeeeeeeee')
 })
