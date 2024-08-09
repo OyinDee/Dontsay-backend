36ee3db5199e78c732529e9edf7569951a954e73
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-08-09 23:21:22 +0100

    Update Messages.model.js

--- Models/Messages.model.js
+++ Models/Messages.model.js
@@ -3,7 +3,7 @@ const messagesSchema = mongoose.Schema({
     message: {type: String, required: true},
     username: {type: String, required: true},
     imageURL: String,
-    time: {type: Date, default: Date.now()}
+    time: String
 });
 const messagesModel = mongoose.model("messages", messagesSchema);
 module.exports={messagesModel};
