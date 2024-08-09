9e08475a937d79c52b25b611700323e3e065f51d
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-09 16:45:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
-app.use(express.urlencoded({limit: '50mb'}))
+app.use(express.urlencoded({limit: '50mb', extended: true}))
 mongoose.Query.prototype.timeout = 20000
 mongoose.connect(url, (err)=>{
     if (err) {
