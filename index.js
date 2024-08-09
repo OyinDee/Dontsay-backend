45f078176a02d0aedabe39071e6c66e6bd3ff4f3
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-09 16:54:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
 app.use(express.urlencoded({limit: '50mb', extended: true}))
-// mongoose.Query.prototype.timeout = 20000
-// mongoose.connect(url, (err)=>{
-//     if (err) {
-//         console.log(err.message);
-//         console.log("Error in mongodb connection");
-//     }
-//     else{
-//         console.log("MONGODB has connected");
-//     }
-// })
+mongoose.Query.prototype.timeout = 20000
+mongoose.connect(url, (err)=>{
+    if (err) {
+        console.log(err.message);
+        console.log("Error in mongodb connection");
+    }
+    else{
+        console.log("MONGODB has connected");
+    }
+})
 
 app.post("/user/create", create);
 app.post("/user/login", login);
